d7a8269b2b3a44c9f3e8dcd52478bb6a5b1c5443
Adan Florez <adan.florezb@gmail.com>
2021-12-03 13:37:13 -0500

    nuevos cambios

--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -3,47 +3,37 @@ import * as ReactBootStrap from "react-bootstrap";
 import { CardProduct } from "./UI/CardProduct";
 
 // import { act } from "react-dom/test-utils";
-export const People = () => {
-  const [people, setPeople] = useState([]);
+export const Products = () => {
+  const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [actual, setActual] = useState(`https://swapi.dev/api/people/?page=1`);
+  const [actual, setActual] = useState(`http://localhost:3001/listaproducto`);
 
-  const fetchPeople = async (page) => {
+  const fetchProduct = async (page) => {
     return fetch(page);
   };
   useEffect(() => {
-    fetchPeople(actual, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-    })
+    fetchProduct(actual)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        let { results } = data;
-
-        // console.log(results);
         setLoading(true);
-        setPeople(results);
-        // console.log(previous, actual, next);
+        setProduct(data);
       });
   }, [actual]);
 
   return (
-      <div className="container-fluid body_info">
-        <div className="box">
-          {loading ? (
-            people.map((people) => {
-              // console.log(url);
-              return <CardProduct people={people} key={people.url} />;
-            })
-          ) : (
-            <ReactBootStrap.Spinner animation="grow" variant="warning" />
-          )}
-          {}
-        </div>
+    <div className="container-fluid body_info">
+      <div className="box">
+        {loading ? (
+          product.map((product) => {
+            return <CardProduct product={product} key={product.url} />;
+          })
+        ) : (
+          <ReactBootStrap.Spinner animation="grow" variant="warning" />
+        )}
+        {}
       </div>
+    </div>
   );
 };
--- src/componentes/screens/ProductScreen.js
+++ src/componentes/screens/ProductScreen.js
@@ -1,22 +1,11 @@
-import React, { Fragment } from "react";
-import './ProductScreen.css';
+import React, { Fragment, useEffect, useState } from "react";
+import "./ProductScreen.css";
 import { useParams, useHistory } from "react-router-dom";
-import { Routers } from "../../rourters/Routers";
 import { CardComments } from "../UI/CardComments";
-import { Comments } from "../vistas/comments";
+import Placeholder from "../../assets/img/placeholder.jpeg";
+
 export const ProductScreen = () => {
-  // let params = useParams();
-  // console.log(match.params.name);
-  const {
-    name,
-    birth_year,
-    height,
-    mass,
-    hair_color,
-    skin_color,
-    eye_color,
-    gender,
-  } = useParams();
+  const { productonombre } = useParams();
   let history = useHistory();
 
   const handleReturn = () => {
@@ -26,101 +15,156 @@ export const ProductScreen = () => {
       history.goBack("/");
     }
   };
+
+  const [product, setProduct] = useState(null);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    try {
+      const getProduct = async () => {
+        const res = await fetch(
+          `http://localhost:3001/listaproducto?productonombre=${productonombre}`
+        );
+        const data = await res.json();
+        setProduct(data[0]);
+        getProductComments(data[0]);
+      };
+      const getProductComments = async (product) => {
+        const res = await fetch(
+          `http://localhost:3001/listacomentarios?idproducto=${product.idproducto}`
+        );
+        const data = await res.json();
+        console.log("data", data);
+        setComments(data);
+      };
+      getProduct();
+    } catch (error) {}
+  }, [productonombre]);
+
   return (
     <Fragment>
       <div className="App">
+        {product ? (
+          <section className={"section-content padding-y bg"}>
+            <div className="container">
+              <article className="card">
+                <div className={"card-body"}>
+                  <div className={"row"}>
+                    <aside className={"col-md-6"}>
+                      <article className={"gallery-wrap"}>
+                        <div className={"card img-big-wrap"}>
+                          <a href="#!">
+                            <img
+                              alt={product.productonombre}
+                              src={
+                                product.imagen ? product.imagen : Placeholder
+                              }
+                            />
+                          </a>
+                        </div>
+                        <div className={"thumbs-wrap"}>
+                          <a href="#" className={"item-thumb"}>
+                            {" "}
+                            <img src={"images/caja.png"} />
+                          </a>
+                          <a href="#" className={"item-thumb"}>
+                            {" "}
+                            <img src={"assets/3.jpg"} />
+                          </a>
+                          <a href="#" className={"item-thumb"}>
+                            {" "}
+                            <img src={"assets/3.jpg"} />
+                          </a>
+                          <a href="#" className={"item-thumb"}>
+                            {" "}
+                            <img src={"assets/3.jpg"} />
+                          </a>
+                        </div>
+                      </article>
+                    </aside>
+                    <main className={"col-md-6"}>
+                      <article>
+                        <a href="#" className={"text-primary btn-link"}>
+                          Categoría
+                        </a>
+                        <h3 className="title"> {product.productonombre} </h3>
+                        <div>
+                          <ul className={"rating-stars"}>
+                            <li className={"stars-active"}>
+                              <i className={"fa fa-star"}></i>{" "}
+                              <i className={"fa fa-star"}></i>
+                              <i className={"fa fa-star"}></i>
+                            </li>
+                            <li>
+                              <i className={"fa fa-star"}></i>{" "}
+                              <i className={"fa fa-star"}></i>
+                              <i className={"fa fa-star"}></i>{" "}
+                              <i className={"fa fa-star"}></i>
+                              <i className={"fa fa-star"}></i>
+                            </li>
+                          </ul>
+                          <span className={"label-rating mr-3 text-muted"}>
+                            10/10
+                          </span>
+                        </div>
 
-        <section className={"section-content padding-y bg"}>
-          <div className="container">
+                        <hr />
 
-            <article className="card">
-              <div className={"card-body"}>
-                <div className={"row"}>
-                  <aside className={"col-md-6"}>
-                    <article className={"gallery-wrap"}>
-                      <div className={"card img-big-wrap"}>
-                        <a href="#"> <img src={"assets/descarga.jfif"} /></a>
-                      </div>
-                      <div className={"thumbs-wrap"}>
-                        <a href="#" className={"item-thumb"}> <img src={"images/caja.png"} /></a>
-                        <a href="#" className={"item-thumb"}> <img src={"assets/3.jpg"} /></a>
-                        <a href="#" className={"item-thumb"}> <img src={"assets/3.jpg"} /></a>
-                        <a href="#" className={"item-thumb"}> <img src={"assets/3.jpg"} /></a>
-                      </div>
-                    </article>
-                  </aside>
-                  <main className={"col-md-6"}>
-                    <article>
-                      <a href="#" className={"text-primary btn-link"}>Categoría</a>
-                      <h3 className="title"> Product title </h3>
-                      <div>
-                        <ul className={"rating-stars"}>
-                          <li className={"stars-active"}>
-                            <i className={"fa fa-star"}></i> <i className={"fa fa-star"}></i>
-                            <i className={"fa fa-star"}></i>
+                        <div className={"mb-3"}>
+                          <h6>Resumen</h6>
+                          <ul className={"list-dots mb-0"}>
+                            <li>{product.descripcion}</li>
+                          </ul>
+                        </div>
 
-                          </li>
-                          <li>
-                            <i className={"fa fa-star"}></i> <i className={"fa fa-star"}></i>
-                            <i className={"fa fa-star"}></i> <i className={"fa fa-star"}></i>
-                            <i className={"fa fa-star"}></i>
-                          </li>
-                        </ul>
-                        <span className={"label-rating mr-3 text-muted"}>10/10</span>
+                        <div className={"mb-3"}>
+                          <h6>Marca</h6>
+                          <ul className={"list-dots mb-0"}>
+                            <li>{product.marca}</li>
+                          </ul>
+                        </div>
 
-                      </div>
+                        <div className="mb-3">
+                          <var className="price h4">$ {product.valor} </var>{" "}
+                          <br />
+                        </div>
 
-                      <hr />
-
-                      <div className={"mb-3"}>
-                        <h6>Resumen</h6>
-                        <ul className={"list-dots mb-0"}>
-                          <li>Valor 1</li>
-                          <li>Valor 2 </li>
-                          <li>Valor 3</li>
-                          <li>Valor 4</li>
-                        </ul>
-                      </div>
-
-
-
-                      <div className="mb-3">
-                        <var className="price h4">$ </var> <br />
-
-                      </div>
-
-                      <div className="mb-4">
-                        <a href="#" className="btn btn-info mr-1">Comprar</a>
-                        <a href="#" className="btn btn-light">Agregar al carrito</a>
-                      </div>
-
-                    </article>
-                  </main>
-                </div>
-              </div>
-            </article>
-            <article className="card mt-5">
-              <div className="card-body">
-                <div className="row">
-                 
-
-                  <div className="col-md-12 ">
-                    <h5>Descripción</h5>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                      consequat.
-                    </p>
+                        <div className="mb-4">
+                          <a href="#" className="btn btn-info mr-1">
+                            Comprar
+                          </a>
+                          <a href="#" className="btn btn-light">
+                            Agregar al carrito
+                          </a>
+                        </div>
+                      </article>
+                    </main>
                   </div>
                 </div>
-                <hr />
+              </article>
+              <article className="card mt-5">
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-md-12 ">
+                      <h5>Descripción</h5>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat.
+                      </p>
+                    </div>
+                  </div>
+                  <hr />
+                </div>
+              </article>
 
-              </div>
-            </article>
-
-            <h2>Comentarios</h2>
-            {/* <div>
+              <h2>Comentarios</h2>
+              {comments.map((comment) => (
+                <CardComments comment={comment} />
+              ))}
+              {/* <div>
               <label>Comenta:</label>
               <div>
                 <form onSubmit={(e) => {
@@ -145,14 +189,10 @@ export const ProductScreen = () => {
               </div>)
             }
 
-          // </div> */}
-          </div>
-        </section>
-
-
-
-
-
+        </div> */}
+            </div>
+          </section>
+        ) : null}
       </div>
       {/* <div className="container screens container-screens bordered rounded mt-4  animate__animated  animate__fadeIn">
         <div className="row">
@@ -238,15 +278,7 @@ export const ProductScreen = () => {
             }
 
           // </div> */}
-          </div>
 
-        </section>
-
-
-
-
-
-      </div>
       {/* <div class="factura">
       <div class="panel-body">
                     <div class="table-responsive">
@@ -301,18 +333,6 @@ export const ProductScreen = () => {
                     </div>
                 </div>
               </div> */}
-    
-     
-          <button
-            className="justify-content-center py-2 mb-3 "
-            onClick={handleReturn}
-          >
-            Pagar
-          </button>
-        </div>
-      </div> */}
-
-      <Comments />
     </Fragment>
   );
 };
